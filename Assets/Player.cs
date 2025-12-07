135dd6c7d6e32814bf21246026b30cd0b4d31656
Lord Vinja <LordVinja@VersionInfinity.com>
2025-12-07 15:03:46 -0500

    fix(NonCourseRelated): Cleaned out code missed in previous ciommit and removed tags

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,31 +2,4 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D Rb;
-    public string PlayerName = "Baller";
-    public int Age = 45;
-    public float MoveSpeed = 4.35f;
-    public bool IsAlive = true;
-    [SerializeField] private int score = 0;
-
-    public int currentHp = 100;
-
-    void Update()
-    {
-        Rb.linearVelocity = new Vector2(Input.GetAxis("Horizontal"), Rb.linearVelocityY);
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-            currentHp -= collision.gameObject.GetComponent<Damage>().GetDamage();
-            if (currentHp <= 0)
-            {
-                IsAlive = false;
-                Debug.Log($"{PlayerName} has died.");
-            }
-        }
-    }
-
 }
