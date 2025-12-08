bf99b6ebf96405e5b76cf2a7e52471dfa4385cdc
Lord Vinja <LordVinja@VersionInfinity.com>
2025-12-08 16:17:51 -0500

    feat: Added raw jump based on inputs refactor: movement conveerted into methods note: method sctucture doesn't match course video #12 so we will see how that plays out.

--- Assets/Constants.cs
+++ Assets/Constants.cs
@@ -3,4 +3,5 @@ using UnityEngine;
 public class Constants 
 {
     public const string HORIZONTAL_INPUT = "Horizontal";
+    public const string VERTICAL_INPUT = "Vertical";
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,8 +4,10 @@ public class Player : MonoBehaviour
 {
     [SerializeField] public Rigidbody2D rb;
     private float xInput;
+    private float yInput;
     [SerializeField] private float moveSpeed = 3.5f;
     [SerializeField] private bool instantAcceleration = true;
+    [SerializeField] private float jumpForce = 8;
 
     private void Awake()
     {
@@ -17,10 +19,33 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        Move();
+
+        if (JumpInputDetected())
+            Jump();
+    }
+
+    private void Move()
+    {
         //Course directs to use GetAxisRaw for instant input response and GetAxis for smoothed input so we allow both
-        xInput = (instantAcceleration ? Input.GetAxisRaw(Constants.HORIZONTAL_INPUT): Input.GetAxis(Constants.HORIZONTAL_INPUT));
+        xInput = (instantAcceleration ? Input.GetAxisRaw(Constants.HORIZONTAL_INPUT) : Input.GetAxis(Constants.HORIZONTAL_INPUT));
 
         //Course directs to create a new Vector2 each frame, but this is more efficient
         rb.linearVelocityX = xInput * moveSpeed;
+    }
+    private static bool JumpInputDetected()
+    {
+        return Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetMouseButtonDown(1);
+    }
+
+
+    private void Jump()
+    {
+        rb.linearVelocityY += jumpForce;
     }
 }
