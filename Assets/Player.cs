ed82c4cd989b83db708f3aa68b97811bfd57e85e
Lord Vinja <LordVinja@VersionInfinity.com>
2025-12-08 22:40:02 -0500

    feat: Handle player orientation and rotate to face expected direction Note: My approach to handling when to rotate is based on managing the characters current direction and comparing it to a rotation flag. This isn't how the course video #15 handles horizontal rotation.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,25 +1,59 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] public Rigidbody2D rb;
     private float xInput;
-    private float yInput;
+    private bool rotatedLeft = false;
+
+    [SerializeField] private CharacterDirection currentDirection = CharacterDirection.Right;
     [SerializeField] private float moveSpeed = 3.5f;
     [SerializeField] private bool instantAcceleration = true;
     [SerializeField] private float jumpForce = 8;
 
+    public Rigidbody2D body;
+    public Animator animator;
+
+    private enum CharacterDirection
+    {
+        Left,
+        Right,
+        Up,
+        Down
+    }
+
     private void Awake()
     {
-        if (rb == null)
-        {
-            rb = GetComponent<Rigidbody2D>();
-        }
+        if (body == null)
+            body = GetComponent<Rigidbody2D>();
+        
+        if (animator == null)
+            animator = GetComponentInChildren<Animator>();
+
+        //Ensure player is facing the correct direction on start
+        HandleOrientation();
     }
 
     private void Update()
     {
         HandleMovement();
+        HandleAnimation();
+        HandleOrientation();
+    }
+
+    [ContextMenu("Flip Player")]
+    private void HandleOrientation()
+    {
+        if ((currentDirection == CharacterDirection.Left && !rotatedLeft) || (currentDirection != CharacterDirection.Left && rotatedLeft))
+        {
+            transform.Rotate(0, 180, 0);
+            rotatedLeft = !rotatedLeft;
+        }
+    }
+
+    private void HandleAnimation()
+    { 
+        animator.SetBool("isMoving", xInput != 0);
     }
 
     private void HandleMovement()
@@ -34,18 +68,21 @@ public class Player : MonoBehaviour
     {
         //Course directs to use GetAxisRaw for instant input response and GetAxis for smoothed input so we allow both
         xInput = (instantAcceleration ? Input.GetAxisRaw(Constants.HORIZONTAL_INPUT) : Input.GetAxis(Constants.HORIZONTAL_INPUT));
+        
+        if(xInput != 0)
+            currentDirection = xInput < 0 ? CharacterDirection.Left : CharacterDirection.Right;
 
         //Course directs to create a new Vector2 each frame, but this is more efficient
-        rb.linearVelocityX = xInput * moveSpeed;
+        body.linearVelocityX = xInput * moveSpeed;
     }
     private static bool JumpInputDetected()
     {
-        return Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetMouseButtonDown(1);
+        return Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(1);
     }
 
 
     private void Jump()
     {
-        rb.linearVelocityY += jumpForce;
+        body.linearVelocityY += jumpForce;
     }
 }
